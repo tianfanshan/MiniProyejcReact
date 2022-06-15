f7a6dcd2a74c894b03604cde9853e6e8765a3134
tianfanshan <stf30111994@gmail.com>
2022-06-15 13:28:08 +0200

    empezado form en develop

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Form = () => {
+    
   return (
-    <div>Form</div>
+    <div>
+        <form action="">
+            <button type='submit'>
+            <label htmlFor="">Textea nueva noticia</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            </button>
+        </form>
+    </div>
   )
 }
 
