93a66f5dbea59c99a7defe1053e690f0feac20a0
tianfanshan <stf30111994@gmail.com>
2022-06-15 14:04:29 +0200

    form 1.0

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,71 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Form = () => {
-    
+    let newsData = JSON.parse(localStorage.getItem('news')) || []
+    const getData = () =>{
+        newsData.push(data)
+        localStorage.setItem('news',JSON.stringify(newsData))
+    }
+
+    const date = new Date()
+
+    const initialData ={
+        name:'',
+        email:'',
+        news:'',
+        date:date
+    }
+
+    const [data,setData] = useState({...initialData})
+
+    const clearData =()=>{
+        setData(initialData)
+    }
+
+    const handleInputChange =(event)=>{
+        setData({
+            ...data,
+            [event.target.name]:event.target.value
+        })
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        getData()
+        clearData()
+    }
+
   return (
     <div>
-        <form action="">
-            <button type='submit'>
-            <label htmlFor="">Textea nueva noticia</label>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            </button>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="">Tu nombre</label>
+            <input 
+            type="text" 
+            placeholder='Tu nombre'
+            name='name'
+            value={data.name}
+            onChange={handleInputChange}
+            />
+            <br />
+            <label htmlFor="">Tu correo</label>
+            <input 
+            type="email" 
+            placeholder='Tu correo'
+            name='email'
+            value={data.email}
+            onChange={handleInputChange}
+            />
+            <br />
+            <label htmlFor="">Textea nueva noticia</label><br />
+            <textarea 
+            cols="30" rows="10"
+            placeholder='Algo nuevo?'
+            name="news"
+            value={data.news}
+            onChange={handleInputChange}
+            />
+            <input type="submit" />
         </form>
     </div>
   )
